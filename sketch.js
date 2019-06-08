1214130e7b7e3c8d8e43bc74e9dd3979f702334b
prashantsingh7289 <51265169+prashantsingh7289@users.noreply.github.com>
2019-06-08 13:28:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    $('#rob').slideUp(function() {
+        $(this).show('3000');
+    });
+ $('#menu1').ready(function() {
+    $('#menu1').slideUp(function() {
+        $(this).show(3000);
+    });
+});
+$('#menu2').ready(function() {
+    $('#menu2').slideUp(function() {
+        $(this).show(3000);
+    });
+});
+    $('#m1').ready(function() {
+    $('#m1').slideUp(function() {
+        $(this).show(8000);
+    });
+});
+$('#m2').ready(function() {
+    $('#m2').slideUp(function() {
+        $(this).show(8000);
+    });
+});
+    $('#m3').ready(function() {
+    $('#m3').slideUp(function() {
+        $(this).show(8000);
+    });
+});
+    $('#m4').ready(function() {
+    $('#m4').slideUp(function() {
+        $(this).show(8000);
+    });
+});
+    $('#table').ready(function() {
+        $('#table').slideUp(function() {
+            $(this).show(10000);
+        });
+    });
+    $('#rob1').ready(function() {
+        $('#rob1').slideUp(function() {
+            $(this).show(10000);
+        });
+    });
+    $('#rob2').ready(function() {
+        $('#rob2').slideUp(function() {
+            $(this).show(10000);
+        });
+    });
+    
+    });
